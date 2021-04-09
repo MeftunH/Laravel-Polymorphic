7412b0e7de223ae56d7eb0ec6a3bc3f289bbd058
MeftunH <maftunhashimli@gmail.com>
2021-04-09 06:53:43 +0400

    post faker configure Image feature/migrations and model relationship configs

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable=['title','body'];
+    public function image()
+    {
+        return $this->morphOne(Image::class,'imageable');
+    }
 }
